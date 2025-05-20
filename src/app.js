1bb4abda768b1abee6d344608d93a072aa3319e9
abhishekkumar <abhishekkumar345@gmail.com>
2025-05-20 17:32:44 +0530

    connect DB

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/database');
 const app= express();
-app.use("/test",(req,res)=>{
-    res.send('Hello World!');
-})
-app.listen(7777, () => {
-    console.log('Server is running on port 7777...');
-});
+
+connectDB()
+  .then(()=> {
+    console.log('MongoDB connected...');
+    app.listen(7777, () => {
+      console.log('Server is running on port 7777...');
+    });
+  })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    }); 
+ 
+
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    await mongoose.connect( 
+        'mongodb+srv://abhishekkumar345ssm:WbAjCjfUECr5eUJy@namastenode.o68kn.mongodb.net/'
+    );
+};
+
+module.exports = connectDB;
