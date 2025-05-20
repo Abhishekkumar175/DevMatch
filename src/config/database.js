b64c914cc7136eae567d2f0433f945ee337d95c1
abhishekkumar <abhishekkumar345@gmail.com>
2025-05-20 20:28:29 +0530

    db created

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     await mongoose.connect( 
-        'mongodb+srv://abhishekkumar345ssm:WbAjCjfUECr5eUJy@namastenode.o68kn.mongodb.net/'
+       // 'mongodb+srv://abhishekkumar345ssm:WbAjCjfUECr5eUJy@namastenode.o68kn.mongodb.net/'
     );
 };
 
