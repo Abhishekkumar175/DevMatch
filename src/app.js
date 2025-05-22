2f33050c14c97cb268720972edc4323ec94cc7e6
abhishekkumar <abhishekkumar345@gmail.com>
2025-05-22 08:41:07 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const app= express();
+const User = require('./models/user');
+
+app.post("/signup", async (req, res) => {
+    //creating a new instance of the user model
+    
+  try {
+    await user.save();
+    res.send("User Created");
+  } catch(err){
+    res.status(400).send("Error creating user: " + err.message);
+  }
+});
+
+
 
 connectDB()
   .then(()=> {
