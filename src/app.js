b9e71595457bd3f746119662374a15c9e128a8da
abhishekkumar <abhishekkumar345ssm@gmail.com>
2025-05-27 12:52:56 +0530

    fixed bugs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
 const express = require('express');
 const connectDB = require('./config/database');
 const app= express();
-require("dotenv").config();
+const cors = require("cors");
+const http = require("http");
+
 const cookieParser = require("cookie-parser");
+
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,7 +33,7 @@ app.use("/", userRouter);
 connectDB()
   .then(()=> {
     console.log('MongoDB connected...');
-    app.listen(7777, () => {
+    app.listen(process.env.PORT || 7777, () => {
       console.log('Server is running on port 7777....');
     });
   })
